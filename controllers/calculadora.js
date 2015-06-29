b0a7f2007bd33e80d82c3f48ab5ddfc07c3426d9
Gabriel <dallariva.gabriel@gmail.com>
2015-06-28 23:22:39 -0300

    Faltando os testes.

--- controllers/calculadora.js
+++ controllers/calculadora.js
@@ -37,6 +37,90 @@ module.exports = function() {
 				
 				dividir: function(numero1, numero2) {
 					return numero1 / numero2;
+				},
+
+				telhaFrancesa:  function(numero1, numero2){
+					return numero1 * numero2 * 1.6;
+				},
+
+				telhaPaulista: function(numero1, numero2){
+					return numero1 * numero2 * 2.5;
+				},
+
+				telhaPlan: function(numero1, numero2){
+					return numero1 * numero2 * 2.6;
+				},
+
+				tijolosComuns5cm: function(numero1){
+					return numero1 * 46;
+				},
+
+				tijolosComuns10cm: function(numero1){
+					return numero1 * 84;
+				},
+
+				tijolosComuns20cm: function(numero1){
+					return numero1 * 149;
+				},
+
+				tijolosCeramicos10cm: function(numero1){
+					return numero1 * 25;
+				},
+
+				tijolosCeramicos20cm: function(numero1){
+					return numero1 * 47;
+				},
+
+				blocosDeConcreto10cm: function(numero1){
+					return numero1 * 14;
+				},
+
+				blocosDeConcreto15cm: function(numero1){
+					return numero1 * 17;
+				},
+
+				blocosDeConcreto20cm: function(numero1){
+					return numero1 * 21;
+				},
+
+				acrilicaEconomica: function(numero1, numero2) {
+					return numero1 * (numero2 / 8.30);
+				},
+
+				acrilicaPremiumAcetinado: function(numero1, numero2) {
+					return numero1 * (numero2 / 10);
+				},
+
+				acrilicaPremiumFosco: function(numero1, numero2) {
+					return numero1 * (numero2 / 21);
+				},
+
+				acrilicaStandard: function(numero1, numero2) {
+					return numero1 * (numero2 / 13.80);
+				},
+
+				esmaltePremium: function(numero1, numero2) {
+					return numero1 * (numero2 / 11);
+				},
+
+				latexPremium: function(numero1, numero2) {
+					return numero1 * (numero2 / 16.60);
+				},
+
+				latexStandard: function(numero1, numero2) {
+					return numero1 * (numero2 / 12.5);
+				},
+
+				tintaParaGesso: function(numero1, numero2) {
+					return numero1 * (numero2 / 6.5);
+				},
+
+				tintaParaPiso: function(numero1, numero2) {
+					return numero1 * (numero2 / 5.5);
+				},
+
+				vernizAcrilico: function(numero1, numero2) {
+					return numero1 * (numero2 / 12.5);
 				}
 			},
 			
@@ -81,6 +165,90 @@ module.exports = function() {
 			
 			dividir: function(request, response) {
 				response.render('dividir');
+			},
+
+			telhaFrancesa: function(request, response){
+				response.render('telhaFrancesa');
+			},
+
+			telhaPaulista: function(request, response){
+				response.render('telhaPaulista');
+			},
+
+			telhaPlan: function(request, response){
+				response.render('telhaPlan');
+			},
+
+			tijolosComuns5cm: function(request, response){
+				response.render('tijolosComuns5cm');
+			},
+
+			tijolosComuns10cm: function(request, response){
+				response.render('tijolosComuns10cm');
+			},
+
+			tijolosComuns20cm: function(request, response){
+				response.render('tijolosComuns20cm');
+			},
+
+			tijolosCeramicos10cm: function(request, response){
+				response.render('tijolosCeramicos10cm');
+			},
+
+			tijolosCeramicos20cm: function(request, response){
+				response.render('tijolosCeramicos20cm');
+			},
+
+			blocosDeConcreto10cm: function(request, response){
+				response.render('tijolosCeramicos10cm');
+			},
+
+			blocosDeConcreto15cm: function(request, response){
+				response.render('blocosDeConcreto15cm');
+			},
+
+			blocosDeConcreto20cm: function(request, response){
+				response.render('tijolosCeramicos20cm');
+			},
+
+			acrilicaEconomica: function(request, response){
+				response.render('acrilicaEconomica');
+			},
+
+			acrilicaPremiumAcetinado: function(request, response){
+				response.render('acrilicaPremiumAcetinado');
+			},
+
+			acrilicaPremiumFosco: function(request, response){
+				response.render('acrilicaPremiumFosco');
+			},
+
+			acrilicaStandard: function(request, response){
+				response.render('acrilicaStandard');
+			},
+
+			esmaltePremium: function(request, response){
+				response.render('esmaltePremium');
+			},
+
+			latexPremium: function(request, response){
+				response.render('latexPremium');
+			},
+
+			latexStandard: function(request, response){
+				response.render('latexStandard');
+			},
+
+			tintaParaGesso: function(request, response){
+				response.render('tintaParaGesso');
+			},
+
+			tintaParaPiso: function(request, response){
+				response.render('tintaParaPiso');
+			},
+
+			vernizAcrilico: function(request, response){
+				response.render('vernizAcrilico');
 			}
 		},
 		
